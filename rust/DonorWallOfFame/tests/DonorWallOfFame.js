1c96c95e23ec093c74a41ac2a7af12687a7b65ee
Juan Ruiz de Bustillo <ctlndjuan@hotmail.com>
2021-11-12 13:15:49 +0100

    rust not ready yet

--- rust/DonorWallOfFame/tests/DonorWallOfFame.js
+++ rust/DonorWallOfFame/tests/DonorWallOfFame.js
@@ -2,12 +2,10 @@ const anchor = require('@project-serum/anchor');
 const solana = require("@solana/web3.js");
 const splToken = require("@solana/spl-token");
 const fetch = require('node-fetch')
-const borsh = require('borsh')
+const borsh = require('borsh');
 
 const { LAMPORTS_PER_SOL, SYSVAR_CLOCK_PUBKEY } = solana;
 const { Keypair, SystemProgram, PublicKey } = anchor.web3;
-
-const kp = {"_keypair":{"publicKey":{"0":13,"1":9,"2":88,"3":118,"4":47,"5":62,"6":139,"7":132,"8":66,"9":107,"10":73,"11":212,"12":150,"13":192,"14":100,"15":60,"16":217,"17":220,"18":245,"19":108,"20":115,"21":249,"22":117,"23":133,"24":246,"25":57,"26":186,"27":133,"28":214,"29":32,"30":106,"31":95},"secretKey":{"0":131,"1":168,"2":158,"3":78,"4":142,"5":172,"6":24,"7":106,"8":74,"9":118,"10":170,"11":201,"12":87,"13":171,"14":140,"15":3,"16":170,"17":221,"18":205,"19":217,"20":139,"21":185,"22":171,"23":179,"24":56,"25":166,"26":62,"27":157,"28":71,"29":5,"30":60,"31":57,"32":13,"33":9,"34":88,"35":118,"36":47,"37":62,"38":139,"39":132,"40":66,"41":107,"42":73,"43":212,"44":150,"45":192,"46":100,"47":60,"48":217,"49":220,"50":245,"51":108,"52":115,"53":249,"54":117,"55":133,"56":246,"57":57,"58":186,"59":133,"60":214,"61":32,"62":106,"63":95}}}
 
 const main = async() => {
   console.log("🚀 Starting test...")
@@ -20,64 +18,97 @@ const main = async() => {
     // Create an account keypair for our program to use.
   const baseAccount = anchor.web3.Keypair.generate();
 
+/*   const kp = {"_keypair":{"publicKey":{"0":13,"1":9,"2":88,"3":118,"4":47,"5":62,"6":139,"7":132,"8":66,"9":107,"10":73,"11":212,"12":150,"13":192,"14":100,"15":60,"16":217,"17":220,"18":245,"19":108,"20":115,"21":249,"22":117,"23":133,"24":246,"25":57,"26":186,"27":133,"28":214,"29":32,"30":106,"31":95},"secretKey":{"0":131,"1":168,"2":158,"3":78,"4":142,"5":172,"6":24,"7":106,"8":74,"9":118,"10":170,"11":201,"12":87,"13":171,"14":140,"15":3,"16":170,"17":221,"18":205,"19":217,"20":139,"21":185,"22":171,"23":179,"24":56,"25":166,"26":62,"27":157,"28":71,"29":5,"30":60,"31":57,"32":13,"33":9,"34":88,"35":118,"36":47,"37":62,"38":139,"39":132,"40":66,"41":107,"42":73,"43":212,"44":150,"45":192,"46":100,"47":60,"48":217,"49":220,"50":245,"51":108,"52":115,"53":249,"54":117,"55":133,"56":246,"57":57,"58":186,"59":133,"60":214,"61":32,"62":106,"63":95}}}
   const arr = Object.values(kp._keypair.secretKey)
   const secret = new Uint8Array(arr)
-  const user = anchor.web3.Keypair.fromSecretKey(secret)
+  const user = anchor.web3.Keypair.fromSecretKey(secret) */
 
-  const receiver = anchor.web3.Keypair.fromSecretKey(secret)
-  /**
-   * airdrop - Airdrops SOL to an account.
-   *
-   * @param {PublicKey} publicKey
-   */
-   async function airdrop(publicKey) {
-    await provider.connection
-      .requestAirdrop(publicKey, LAMPORTS_PER_SOL)
-      .then((sig) => provider.connection.confirmTransaction(sig, "confirmed"));
-  }
-  console.log("🌭 Airdropping 1 SOL...")
-  await airdrop(provider.wallet.publicKey);
+  const user = anchor.web3.Keypair.generate();
+  console.log("User wallet", user.publicKey.toBase58())
+
+  const bigAccount = anchor.web3.Keypair.generate();
+  const receiver = anchor.web3.Keypair.generate();
+
+  let ix = await createBigAccount(
+    program,
+    user,
+    bigAccount,
+    provider.connection
+  )
+ 
+  let airdrop_amount = 25;
+  console.log("🌭 Airdropping", airdrop_amount, "SOL...")
+  await airdrop(user.publicKey, airdrop_amount, provider);
   console.log("airdropped")
   // Call start_stuff_off, pass it the params it needs!
+
+  const [_pda, _nonce] = await PublicKey.findProgramAddress(
+    [Buffer.from(anchor.utils.bytes.utf8.encode("donor"))],
+    program.programId
+  );
+
+  console.log("found pda", _pda.toBase58())
+
   let tx = await program.rpc.entryPoint({
     accounts: {
-      baseAccount: baseAccount.publicKey,
-      user: provider.wallet.publicKey,
+      stateAccount: _pda,
+      baseAccount: bigAccount.publicKey,
+      user: user.publicKey,
       systemProgram: SystemProgram.programId,
     },
-    signers: [baseAccount],
+    instructions: [
+      ix
+    ],
+    signers: [bigAccount, user]
   });
 
   console.log("📝 Your transaction signature", tx);
 
   // Fetch data from the account.
-  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-/*   console.log('👀 Donor Count', account.totalDonors.toString())
+  //let account = await program.account.baseAccount.fetch(bigAccount.publicKey);
+
+  let stateAccount = await program.account.stateAccount.fetch(_pda)
+  let account = await program.account.baseAccount.fetch(stateAccount.baseAccount)
+
+ /*  console.log("👀 BIGACCOUNT", stateAccount.baseAccount.toString())
+  console.log('👀 Donor Count', account.totalDonors.toString()) */
     
   let random_nft = new anchor.web3.PublicKey("6b8ropjXu1tsc6gL6ZpS9XacEvNtoySJ2BCYZTHeVwja")
   let random_nft_2 = new anchor.web3.PublicKey("6b8ropjXu1tsc6gL6ZpS9XacEvNtoySJ2BCYZTHeVwjb")
-  // Call add_gif!
-  await program.rpc.addDonor(
-    "@if__name__main", 
-    "Joan Ruiz de Bustillo",
-    5,
-    random_nft,
-    10,
-    true,
-    "test_arweave_link",
-    provider.wallet.publicKey, {
-    accounts: {
-      baseAccount: baseAccount.publicKey,
-      clock: anchor.web3.SYSVAR_CLOCK_PUBKEY
-    },
-  });
+
+  for (let i = 0; i < 1000; i++) {
+
+    const donor_account = anchor.web3.Keypair.generate();
+    
+    /* console.log("Accouts", account.publicKey, stateAccount.publicKey) */
+    //let account = await program.account.baseAccount.fetch(stateAccount.baseAccount)
+    console.log("👀 Donated ", i, "times"/* . Total donors", account.totalDonors.toString() */)
+    // Call add_gif!
+    await program.rpc.addDonor(
+      "@"+i.toString(), 
+      "Donor" + i.toString(),
+      new anchor.BN(5),
+      random_nft,
+      new anchor.BN(10),
+      true,
+      "test_arweave_link",
+      donor_account.publicKey, 
+      {
+      accounts: {
+        baseAccount: stateAccount.baseAccount,//baseAccount.publicKey,
+        stateAccount: _pda,
+        clock: anchor.web3.SYSVAR_CLOCK_PUBKEY
+      },
+    });
+    //await new Promise((resolve) => setTimeout(resolve, 1000))
+  }
   
   await program.rpc.addDonor(
     "@if__name__main", 
     "Joan Ruiz de Bustillo",
-    10,
+    new anchor.BN(10 * LAMPORTS_PER_SOL),
     random_nft,
-    20,
+    new anchor.BN(20 * LAMPORTS_PER_SOL),
     true,
     "test_arweave_link",
     provider.wallet.publicKey, {
@@ -93,7 +124,7 @@ const main = async() => {
 
   console.log("👀 Donor List", account.donorList)
 
-  console.log("Donated tokens", account.donorList[0]) */
+  console.log("Donated tokens", account.donorList[0])
 
   let my_balance = await provider.connection.getBalance(user.publicKey)
   const amount = 1000;
@@ -152,10 +183,10 @@ const main = async() => {
   console.log(`⛽ Test passed: Account ${initializeRandomMint} has  ${_randomInfoAccount.amount} tokens and ${my_balance/ LAMPORTS_PER_SOL} SOL. 
             Other Account ${initializeRandomMintOnOtherAccount} has ${_randomInfoAccountOnOtherAccount.amount} Tokens`)
 
-  const [_pda, _nonce] = await PublicKey.findProgramAddress(
+/*   const [_pda, _nonce] = await PublicKey.findProgramAddress(
     [Buffer.from(anchor.utils.bytes.utf8.encode("donor"))],
     program.programId
-  );
+  ); */
 
   // console.log("Bump is ", _nonce) 
 
@@ -252,5 +283,37 @@ async function fetchTokenAccount(address, program) {
   return splToken.AccountLayout.decode(tokenAccountInfo.data);
 }
 
+async function createBigAccount(
+  anchorProgram,
+  payerWallet,
+  bigAccount,
+  connection
+) {
+  const size = 5_000
+
+  let ix = anchor.web3.SystemProgram.createAccount({
+    fromPubkey: payerWallet.publicKey,
+    newAccountPubkey: bigAccount.publicKey,
+    space: size,
+    lamports:
+      await anchorProgram.provider.connection.getMinimumBalanceForRentExemption(
+        size,
+      ),
+    programId: new PublicKey("7WhXtFrss1nPQiFN87JzQRpFHSm6dt792BrTc6cFbZSm"),
+  });
+
+  return ix;
+}
+
+/**
+ * airdrop - Airdrops SOL to an account.
+ *
+ * @param {PublicKey} publicKey
+ */
+async function airdrop(publicKey, amount, provider) {
+  await provider.connection
+    .requestAirdrop(publicKey, amount * LAMPORTS_PER_SOL)
+    .then((sig) => provider.connection.confirmTransaction(sig, "confirmed"));
+}
 
 runMain();
